e7ffcea8588ef185bd3ca6c709f0fed66167161a
Daniel LaGrotta <soul2197@gmail.com>
2016-03-14 14:57:43 -0700

    Remove error handler

--- server.js
+++ server.js
@@ -81,10 +81,6 @@ app.get('/chart.png', function(request, response) {
     });
   })
 });
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send(err);
-});
 app.listen(process.env.PORT || 2197);
 
 function generateChart(request, callback) {
